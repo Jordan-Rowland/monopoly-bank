415a71932d35ad6c2de83b7c2d0b75015fedff20
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-09-22 18:30:36 -0700

    fixes bug, extends menu

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from itertools import cycle
+from time import sleep
+
 
 class Game:
     def __init__(self, players):
@@ -18,61 +20,98 @@ class Game:
 
     def menu(self, player):
         print()
-        for player in self.players:
-            print(f'{player.name} - ${player.money}', end='\t')
-        print(f'\n{"=" * 75}')
-        print(f'Current turn: {player.name} - ${player.money}\n')
-        print(
-            "Choose an action:\n"
-            "(P) Pay...\n"
-            "(C) Collect money\n"
-            "(E) End turn\n"
-        )
-        action = input('> ').lower()
-        if action == 'p':
-            self.pay_menu(player)
-        elif action == 'c':
-            pass
-        elif action == 'e':
-            pass
+        while True:
+            print(f'Community pot - ${self.community_pot}')
+            for iplayer in self.players:
+                print(f'{iplayer.name} - ${iplayer.money}', end='\t')
+            print(f'\n{"=" * 75}')
+            print(f'Current turn: {player.name} - ${player.money}\n')
+            print(
+                "Choose an action:\n"
+                "(P) Pay...\n"
+                "(C) Collect money\n"
+                "(G) Pass Go(collect $200)\n"
+                "(E) End turn\n"
+            )
+            action = input('> ').lower()
+            if action == 'p':
+                self.pay_menu(player)
+                print()
+            elif action == 'c':
+                self.collect_menu(player)
+                print()
+            elif action == 'g':
+                player.receive_from_bank(200)
+            elif action == 'e':
+                break
 
     def pay_menu(self, player):
         print(f'\n{"=" * 25}')
         amount = None
-        while not amount:
-            try:
-                amount = int(input('How much?\n> '))
-            except ValueError:
-                print('Must select a numerical value')
+        selected_player = None
         print('Choose who to pay:\n')
         print(
             "(P) Pay Player\n"
             "(B) Pay Bank\n"
             "(C) Pay Community Pot\n"
-            "(E) Pay Everyone"
+            "(E) Pay Everyone\n"
         )
         pay_action = input('> ').lower()
-        if pay_action == 'b':
+        if pay_action == 'p':
+            selected_player = self.select_user_menu(player)
+        while not amount:
+            try:
+                amount = int(input('How much?\n> '))
+            except ValueError:
+                print('Must select a numerical value')
+        if selected_player:
+            player.pay_player(selected_player, amount)
+        elif pay_action == 'b':
             player.pay_bank(amount)
         elif pay_action == 'c':
             self.pay_community_pot(player, amount)
-        elif pay_action == 'p':
-            selected_player = self.select_user_menu(player)
-            player.pay_player(selected_player, amount)
         elif pay_action == 'e':
             player.pay_all_players(self.players, amount)
 
-    def select_user_menu(self, player):
+    def collect_menu(self, player):
         print(f'\n{"=" * 25}')
-        print('Select a player:\n')
-        for other_player in self.players:
-            if other_player != player:
-                print(other_player.name)
-        selected_name = input('> ').lower()
-        selected_player = [player for player in game.players
-                            if player.name.lower() == selected_name][0]
+        amount = None
+        print('Collect from:\n')
+        print(
+            "(B) Bank\n"
+            "(C) Community Pot\n"
+            "(E) Everyone\n"
+        )
+        collect_action = input('> ').lower()
+        while not amount:
+            try:
+                amount = int(input('How much?\n> '))
+            except ValueError:
+                print('Must select a numerical value')
+        if collect_action == 'b':
+            player.receive_from_bank(amount)
+        elif collect_action == 'c':
+            game.get_community_pot(player)
+        elif collect_action == 'e':
+            player.receive_from_players(self.players, amount)
+
+    def select_user_menu(self, player):
+        selected_player = None
+        while not selected_player:
+            try:
+                print(f'\n{"=" * 25}')
+                print('Select a player:\n')
+                for other_player in self.players:
+                    if other_player != player:
+                        print(other_player.name)
+                selected_name = input('> ').lower()
+                selected_player = [player for player in game.players
+                                    if player.name.lower() == selected_name][0]
+            except IndexError:
+                print()
+                print('Could not find user.')
         return selected_player
-        
+
 
 class Player:
     """Player class, handles state regarding money, name, and properties."""
@@ -81,16 +120,32 @@ class Player:
         self.money = 1500
         self.is_bankrupt = False
 
-    def pay_player(self, player, amount):
-        self.money -= amount
-        player.money += amount
-        print(f'{self.name} paid {player.name} ${amount}.')
-
     def pay_bank(self, amount):
-        self.money -= amount
-        print(f'{self.name} paid the bank ${amount}.')
+        if self.money > amount:
+            self.money -= amount
+            print(f'{self.name} paid the bank ${amount}.')
+        else:
+            print()
+            print(
+                f"{self.name} does not have enough money for this transaction.\n"
+                "Please sell some assets, or declare bankruptcy"
+            )
+            sleep(1)
 
-    def receive_money(self, amount):
+    def pay_player(self, player, amount):
+        if self.money > amount:
+            self.money -= amount
+            player.money += amount
+            print(f'{self.name} paid {player.name} ${amount}.')
+        else:
+            print()
+            print(
+                f"{self.name} does not have enough money for this transaction.\n"
+                "Please sell some assets, or declare bankruptcy"
+            )
+            sleep(1)
+
+    def receive_from_bank(self, amount):
         self.money += amount
         print(f'{self.name} received ${amount}.')
     
@@ -103,10 +158,11 @@ class Player:
             self.pay_player(player, amount)
 
 
-#if __name__ == '__main__':
-p1 = Player('Rick')
-p2 = Player('Jim')
-p3 = Player('Dave')
-game = Game([p1, p2, p3])
-for i in game.players:
-    game.menu(i)
+if __name__ == '__main__':
+    p1 = Player('Rick')
+    p2 = Player('Jim')
+    p3 = Player('Dave')
+    game = Game([p1, p2, p3])
+    for player in cycle(game.players):
+        if not player.is_bankrupt:
+            game.menu(player)
