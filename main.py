a532615059eea958bf95803cb18f9ab8fdcee945
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-09-25 20:05:33 -0700

    broke menu into it's own module separate from main. Added the ability to add any amount of players

--- main.py
+++ main.py
@@ -1,139 +1,28 @@
-"""Banking system for games of Monopoly board game."""
+"""Main file to run the menu"""
 
 from itertools import cycle
-from time import sleep
 
 from player import Player
 from game import Game
-
-class Menu:
-    def __init__(self, game):
-        self.game = game
-    
-    def menu(self, player):
-        """Displays the top-level menu beginning each players turn."""
-        print()
-        while True:
-            if player.is_bankrupt:
-                return
-            sleep(1)
-            print(f'Community pot - ${self.game.community_pot}')
-            for iplayer in self.game.players:
-                print(f'{iplayer.name} - ${iplayer.money}', end='\t')
-            print(f'\n{"=" * 75}')
-            print(f'Current turn: {player.name} - ${player.money}\n')
-            print("Choose an action:\n")
-            print(
-                "(R) Roll\n"
-                "(P) Pay...\n"
-                "(C) Collect money\n"
-                "(G) Pass Go(collect $200)\n"
-                "(E) End turn\n"
-            )
-            action = input('> ').lower()
-            if action == 'r':
-                self.game.roll()
-            elif action == 'p':
-                self.pay_menu(player)
-                print()
-            elif action == 'c':
-                self.collect_menu(player)
-                print()
-            elif action == 'g':
-                player.collect(amount=200)
-            elif action == 'e':
-                break
-
-    def pay_menu(self, player):
-        """Access this to determine who the current player will pay."""
-        print(f'\n{"=" * 25}')
-        amount = -1
-        selected_player = None
-        print('Choose who to pay:\n')
-        print(
-            "(P) Pay Player\n"
-            "(B) Pay Bank\n"
-            "(C) Pay Community Pot\n"
-            "(E) Pay Everyone\n"
-        )
-        pay_action = input('> ').lower()
-        if pay_action not in ['p', 'b', 'c', 'e',]:
-            print(f'({pay_action.upper()}) is not a value option.')
-            return
-        if pay_action == 'p':
-            selected_player = self.select_user_menu(player)
-        while amount < 0:
-            try:
-                amount = int(input('How much?\n> '))
-            except ValueError:
-                print('Must select a numerical value')
-        if selected_player:
-            player.pay(amount, selected_player)
-        elif pay_action == 'b':
-            player.pay(amount)
-        elif pay_action == 'c':
-            self.game.pay_community_pot(player, amount)
-        elif pay_action == 'e':
-            players = list(filter(lambda x: x != player, self.game.players))
-            player.pay(amount, players)
-
-
-    def collect_menu(self, player):
-        """Access this to determine from who the current player will collect money
-        from."""
-        print(f'\n{"=" * 25}')
-        amount = -1
-        print('Collect from:\n')
-        print(
-            "(C) Community Pot\n"
-            "(B) Bank\n"
-            "(E) Everyone\n"
-        )
-        collect_action = input('> ').lower()
-        if collect_action not in ['c', 'b', 'e',]:
-            print(f'({collect_action.upper()}) is not a value option.')
-            return
-        if collect_action == 'c':
-            amount = self.game.community_pot
-            self.game.get_community_pot(player)
-        while amount < 0:
-            try:
-                amount = int(input('How much?\n> '))
-            except ValueError:
-                print('Must select a numerical value')
-        if collect_action == 'b':
-            player.collect(amount)
-        elif collect_action == 'e':
-            players = list(filter(lambda x: x != player, self.game.players))
-            player.collect(amount, players)
-
-    def select_user_menu(self, player):
-        """Returns the Player object of the player whose name is typed in."""
-        selected_player = None
-        while not selected_player:
-            try:
-                print(f'\n{"=" * 25}')
-                print('Select a player:\n')
-                for other_player in self.game.players:
-                    if other_player != player:
-                        print(other_player.name)
-                selected_name = input('> ').lower()
-                selected_player = next(
-                        filter(lambda x: x.name.lower() == selected_name, self.game.players)
-                        )
-            except StopIteration:
-                print()
-                print('Could not find user.')
-                sleep(1)
-        return selected_player
+from menu import Menu
 
 
 def main():
-    P1 = Player('Rick')
-    P2 = Player('Jim')
-    P3 = Player('Dave')
-    PLAYERS = P1, P2, P3
-    GAME = Game(PLAYERS)
+    num_players = input('How many players?\n> ')
+    player_list = list()
+    while True:
+        try:
+            for i in range(1, int(num_players) + 1):
+                name = input(f'Player {i} name\n> ')
+                player_list.append(Player(name))
+            break
+        except TypeError:
+            print('Must be a valid number')
+#    P1 = Player('Rick')
+#    P2 = Player('Jim')
+#    P3 = Player('Dave')
+#    PLAYERS = P1, P2, P3
+    GAME = Game(player_list)
     MENU = Menu(GAME)
     for game_player in cycle(MENU.game.players):
         if not game_player.is_bankrupt:
---
+++ menu.py
@@ -0,0 +1,125 @@
+"""Menu to control the flow of commands"""
+
+from time import sleep
+
+class Menu:
+    def __init__(self, game):
+        self.game = game
+    
+    def menu(self, player):
+        """Displays the top-level menu beginning each players turn."""
+        print()
+        while True:
+            if player.is_bankrupt:
+                return
+            sleep(1)
+            print(f'Community pot - ${self.game.community_pot}')
+            for iplayer in self.game.players:
+                print(f'{iplayer.name} - ${iplayer.money}', end='\t')
+            print(f'\n{"=" * 75}')
+            print(f'Current turn: {player.name} - ${player.money}\n')
+            print("Choose an action:\n")
+            print(
+                "(R) Roll\n"
+                "(P) Pay...\n"
+                "(C) Collect money\n"
+                "(G) Pass Go(collect $200)\n"
+                "(E) End turn\n"
+            )
+            action = input('> ').lower()
+            if action == 'r':
+                self.game.roll()
+            elif action == 'p':
+                self.pay_menu(player)
+                print()
+            elif action == 'c':
+                self.collect_menu(player)
+                print()
+            elif action == 'g':
+                player.collect(amount=200)
+            elif action == 'e':
+                break
+
+    def pay_menu(self, player):
+        """Access this to determine who the current player will pay."""
+        print(f'\n{"=" * 25}')
+        amount = -1
+        selected_player = None
+        print('Choose who to pay:\n')
+        print(
+            "(P) Pay Player\n"
+            "(B) Pay Bank\n"
+            "(C) Pay Community Pot\n"
+            "(E) Pay Everyone\n"
+        )
+        pay_action = input('> ').lower()
+        if pay_action not in ['p', 'b', 'c', 'e',]:
+            print(f'({pay_action.upper()}) is not a value option.')
+            return
+        if pay_action == 'p':
+            selected_player = self.select_user_menu(player)
+        while amount < 0:
+            try:
+                amount = int(input('How much?\n> '))
+            except ValueError:
+                print('Must select a numerical value')
+        if selected_player:
+            player.pay(amount, selected_player)
+        elif pay_action == 'b':
+            player.pay(amount)
+        elif pay_action == 'c':
+            self.game.pay_community_pot(player, amount)
+        elif pay_action == 'e':
+            players = list(filter(lambda x: x != player, self.game.players))
+            player.pay(amount, players)
+
+
+    def collect_menu(self, player):
+        """Access this to determine from who the current player will collect money
+        from."""
+        print(f'\n{"=" * 25}')
+        amount = -1
+        print('Collect from:\n')
+        print(
+            "(C) Community Pot\n"
+            "(B) Bank\n"
+            "(E) Everyone\n"
+        )
+        collect_action = input('> ').lower()
+        if collect_action not in ['c', 'b', 'e',]:
+            print(f'({collect_action.upper()}) is not a value option.')
+            return
+        if collect_action == 'c':
+            amount = self.game.community_pot
+            self.game.get_community_pot(player)
+        while amount < 0:
+            try:
+                amount = int(input('How much?\n> '))
+            except ValueError:
+                print('Must select a numerical value')
+        if collect_action == 'b':
+            player.collect(amount)
+        elif collect_action == 'e':
+            players = list(filter(lambda x: x != player, self.game.players))
+            player.collect(amount, players)
+
+    def select_user_menu(self, player):
+        """Returns the Player object of the player whose name is typed in."""
+        selected_player = None
+        while not selected_player:
+            try:
+                print(f'\n{"=" * 25}')
+                print('Select a player:\n')
+                for other_player in self.game.players:
+                    if other_player != player:
+                        print(other_player.name)
+                selected_name = input('> ').lower()
+                selected_player = next(
+                        filter(lambda x: x.name.lower() == selected_name, self.game.players)
+                        )
+            except StopIteration:
+                print()
+                print('Could not find user.')
+                sleep(1)
+        return selected_player
+
